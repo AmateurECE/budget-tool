67d2a38757bb760408e11f68a02148962d32778d
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-07-04 16:57:33 -0400

    Make initial_relase migration reversible

---
+++ budget-service/migrations/20220629220930_initial_release.down.sql
@@ -0,0 +1,10 @@
+DROP TABLE accounts;
+DROP TYPE AccountType;
+DROP TABLE periodic_budgets;
+DROP TABLE one_time_budgets;
+DROP TABLE categories;
+DROP TABLE budget_items;
+DROP TABLE transactions;
+DROP TYPE TransactionType;
+DROP TABLE initial_balances;
+DROP FUNCTION update_modified_column;
