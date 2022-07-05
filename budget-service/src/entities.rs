e63a03bbdb904455c5bd50406434210a00ea1f27
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-07-05 08:35:33 -0500

    Update entities module.
    Unfortunately, the persistent model now needs to diverge in a
    significant way from the generated code. It's no longer possible to
    keep this module up-to-date with the schema by generating it, so
    it must be done manually.

---
+++ budget-service/src/entities.rs
@@ -0,0 +1,24 @@
+///////////////////////////////////////////////////////////////////////////////
+// NAME:            entities.rs
+//
+// AUTHOR:          Ethan D. Twardy <ethan.twardy@gmail.com>
+//
+// DESCRIPTION:     Module encapsulating persistent entities
+//
+// CREATED:         07/05/2022
+//
+// LAST EDITED:     07/05/2022
+////
+
+pub mod prelude;
+
+pub mod accounts;
+pub mod budget_items;
+pub mod categories;
+pub mod initial_balances;
+pub mod one_time_budgets;
+pub mod periodic_budgets;
+pub mod sea_orm_active_enums;
+pub mod transactions;
+
+///////////////////////////////////////////////////////////////////////////////
