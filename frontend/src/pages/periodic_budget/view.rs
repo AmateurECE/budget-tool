3e12a95f350aa24442774082b2a8f8683da38853
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-07-15 22:26:18 -0500

    Refactor DataView back into pages::periodic_budget::View

--- frontend/src/pages/periodic_budget/view.rs
+++ frontend/src/pages/periodic_budget/view.rs
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-// NAME:            periodic_budget_view.rs
+// NAME:            view.rs
 //
 // AUTHOR:          Ethan D. Twardy <ethan.twardy@gmail.com>
 //
@@ -10,6 +10,8 @@
 // LAST EDITED:     07/15/2022
 ////
 
+use std::collections::HashMap;
+use budget_models::models::PeriodicBudget;
 use wasm_bindgen_futures::spawn_local;
 use yew::prelude::*;
 
@@ -17,34 +19,40 @@ use crate::network::fetch;
 use crate::{ACCOUNTS_PATH, PERIODIC_BUDGETS_PATH};
 use crate::render::{Render, RenderTable};
 
+use super::account::AccountView;
+use super::budget_item::BudgetItemView;
+
 ///////////////////////////////////////////////////////////////////////////////
 // DataView
 ////
 
-pub struct DataView;
+pub struct DataView {
+    budget: PeriodicBudget,
+    items: HashMap<String, Vec<BudgetItemView>>,
+    accounts: Vec<AccountView>,
+}
 
 impl DataView {
     fn render(&self) -> Html {
-        // html! {<div><h1>{
-        //     &self.budget.start_date
-        // }</h1><h2>{ "Budget" }</h2>{
-        //     self.items.iter().map(|(k, v)| {
-        //         html! {
-        //             <div>
-        //                 <h3>{ k }</h3>
-        //                 <div><table>{ BudgetItemView::header() }{
-        //                     v.iter().map(|item| item.render())
-        //                         .collect::<Html>()
-        //                 }</table></div>
-        //             </div>
-        //         }
-        //     }).collect::<Html>()
-        // }<h2>{ "Accounts" }</h2><table>{ AccountView::header() }{
-        //     self.accounts.iter().map(|account| {
-        //         account.render()
-        //     }).collect::<Html>()
-        // }</table></div>}
-        todo!()
+        html! {<div><h1>{
+            &self.budget.start_date
+        }</h1><h2>{ "Budget" }</h2>{
+            self.items.iter().map(|(k, v)| {
+                html! {
+                    <div>
+                        <h3>{ k }</h3>
+                        <div><table>{ BudgetItemView::header() }{
+                            v.iter().map(|item| item.render())
+                                .collect::<Html>()
+                        }</table></div>
+                    </div>
+                }
+            }).collect::<Html>()
+        }<h2>{ "Accounts" }</h2><table>{ AccountView::header() }{
+            self.accounts.iter().map(|account| {
+                account.render()
+            }).collect::<Html>()
+        }</table></div>}
     }
 }
 
