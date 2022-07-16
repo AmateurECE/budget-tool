97b6262c481f5e8eeb9d907bcad968254f77c908
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-07-16 13:34:16 -0500

    Uncomment BudgetItemView

--- frontend/src/pages/periodic_budget/budget_item.rs
+++ frontend/src/pages/periodic_budget/budget_item.rs
@@ -8,9 +8,10 @@
 //
 // CREATED:         07/15/2022
 //
-// LAST EDITED:     07/15/2022
+// LAST EDITED:     07/16/2022
 ////
 
+use budget_models::{money::Money, models::BudgetItem};
 use yew::prelude::*;
 use crate::render::{Render, RenderTable};
 
@@ -18,51 +19,45 @@ use crate::render::{Render, RenderTable};
 // BudgetItemView
 ////
 
-pub(super) struct BudgetItemView;
+pub(super) struct BudgetItemView {
+    item: BudgetItem,
+    spent: Money,
+}
 
 impl Render for BudgetItemView {
     fn render(&self) -> Html {
-        // let budgeted = format!("{:.2}", (self.0.item.budgeted as f64) / 100.0);
-        // let spent: f64 = self.0.spent.get_total().into();
-        // html! { <tr><td data-label="Description">{
-        //     &self.0.item.description
-        // }</td><td data-label="Transaction Type">{
-        //     &self.0.item.transaction_type.to_string()
-        // }</td><td data-label="From">{
-        //     match &self.0.item.from_account {
-        //         Some(account) => account,
-        //         None => "",
-        //     }
-        // }</td><td data-label="To">{
-        //     match &self.0.item.to_account {
-        //         Some(account) => account,
-        //         None => "",
-        //     }
-        // }</td><td data-label="Budgeted">{
-        //     budgeted
-        // }</td><td data-label="Spent">{
-        //     format!("{:.2}", &spent)
-        // }</td></tr>}
-        todo!()
+        let budgeted: Money = self.item.budgeted.into();
+        html! {<tr><td data-label="Description">{
+            &self.item.description
+        }</td><td data-label="Transaction Type">{
+            &self.item.transaction_type.to_string()
+        }</td><td data-label="From">{
+            self.item.from_account.as_ref().map(|a| a.as_str()).unwrap_or("")
+        }</td><td data-label="To">{
+            self.item.from_account.as_ref().map(|a| a.as_str()).unwrap_or("")
+        }</td><td data-label="Budgeted">{
+            &budgeted.to_string()
+        }</td><td data-label="Spent">{
+            &self.spent.to_string()
+        }</td></tr>}
     }
 }
 
 impl RenderTable for BudgetItemView {
     fn header() -> Html {
-        // html! {<tr><th>{
-        //     "Description"
-        // }</th><th>{
-        //     "Transaction Type"
-        // }</th><th>{
-        //     "From"
-        // }</th><th>{
-        //     "To"
-        // }</th><th>{
-        //     "Budgeted"
-        // }</th><th>{
-        //     "Spent"
-        // }</th></tr>}
-        todo!()
+        html! {<tr><th>{
+            "Description"
+        }</th><th>{
+            "Transaction Type"
+        }</th><th>{
+            "From"
+        }</th><th>{
+            "To"
+        }</th><th>{
+            "Budgeted"
+        }</th><th>{
+            "Spent"
+        }</th></tr>}
     }
 }
 
