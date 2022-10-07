3b3cbc217a532508776e5bebfec026df16755fa4
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-10-07 07:07:32 -0500

    Pull in yew example application for testing build

--- frontend-staging/frontend-staging/src/main.rs
+++ frontend-staging/frontend-staging/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+///////////////////////////////////////////////////////////////////////////////
+// NAME:            main.rs
+//
+// AUTHOR:          Ethan D. Twardy <ethan.twardy@gmail.com>
+//
+// DESCRIPTION:     Entrypoint for the wasm application
+//
+// CREATED:         10/07/2022
+//
+// LAST EDITED:     10/07/2022
+////
+
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
 }
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
+
+///////////////////////////////////////////////////////////////////////////////
