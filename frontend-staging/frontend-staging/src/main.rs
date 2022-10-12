b18e35f1b8cf40596ebf0d2c0b771709be3cec27
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-10-12 07:52:54 -0500

    Minor refactoring for style

--- frontend-staging/frontend-staging/src/main.rs
+++ frontend-staging/frontend-staging/src/main.rs
@@ -7,7 +7,7 @@
 //
 // CREATED:         10/07/2022
 //
-// LAST EDITED:     10/11/2022
+// LAST EDITED:     10/12/2022
 ////
 
 use yew::prelude::*;
@@ -31,18 +31,18 @@ impl FromIterator<String> for FieldSpec {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-// FieldIterator
+// FieldView
 ////
 
 #[derive(Clone, Default, PartialEq)]
-pub struct FieldIterator(Vec<String>);
-impl FieldIterator {
+pub struct FieldView(Vec<String>);
+impl FieldView {
     pub fn iter<'a>(&'a self) -> impl Iterator<Item = &'a String> + 'a {
         self.0.iter()
     }
 }
 
-impl FromIterator<String> for FieldIterator {
+impl FromIterator<String> for FieldView {
     fn from_iter<I: IntoIterator<Item = String>>(iter: I) -> Self {
         Self(iter.into_iter().collect::<Vec<String>>())
     }
@@ -57,7 +57,7 @@ pub trait FieldNames {
 }
 
 pub trait Fields {
-    fn fields(&self) -> FieldIterator;
+    fn fields(&self) -> FieldView;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -66,7 +66,7 @@ pub trait Fields {
 
 #[derive(Properties, PartialEq)]
 pub struct TableRowProps {
-    data: FieldIterator,
+    data: FieldView,
 }
 
 #[function_component]
@@ -83,7 +83,7 @@ fn TableRow(props: &TableRowProps) -> Html {
 
 #[derive(Properties, PartialEq)]
 pub struct TableProps {
-    pub fields: Vec<FieldIterator>,
+    pub fields: Vec<FieldView>,
     pub field_names: FieldSpec,
 }
 
@@ -120,9 +120,10 @@ impl FieldNames for SomeObject {
 }
 
 // TODO: This should be a derive macro
+// TODO: It would be super cool if this could also use Cow<'_>
 impl Fields for SomeObject {
-    fn fields(&self) -> FieldIterator {
-        FieldIterator(vec![self.foo.clone(), self.bar.clone()])
+    fn fields(&self) -> FieldView {
+        FieldView(vec![self.foo.clone(), self.bar.clone()])
     }
 }
 
@@ -133,11 +134,15 @@ impl Fields for SomeObject {
 #[function_component]
 fn App() -> Html {
     let objects = vec![
-        SomeObject { foo: "a".to_string(), bar: "b".to_string() }.fields(),
-        SomeObject { foo: "c".to_string(), bar: "d".to_string() }.fields(),
+        SomeObject { foo: "a".to_string(), bar: "b".to_string() },
+        SomeObject { foo: "c".to_string(), bar: "d".to_string() },
     ];
+
+    let field_spec = SomeObject::field_names();
+    let field_views = objects.iter().map(|object| object.fields())
+        .collect::<Vec<FieldView>>();
     html! {
-        <Table field_names={SomeObject::field_names()} fields={objects} />
+        <Table field_names={field_spec} fields={field_views} />
     }
 }
 
