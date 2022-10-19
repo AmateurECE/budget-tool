c6c6e65d927f9b211f35d2303313e4bf5333dcf8
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-10-18 21:57:00 -0500

    Integrate chart.js to Account Balance History component
    This commit marks the initial integration of Chart.js, which is
    very exciting, because this is the first time I'm pulling in a
    javascript dependency to a Yew application and making calls into it!
    It needs significant cleanup, and preferably a way to separate all
    Chart.js calls into a separate crate

--- frontend-staging/frontend-staging/src/performance/account_balance.rs
+++ frontend-staging/frontend-staging/src/performance/account_balance.rs
@@ -7,15 +7,127 @@
 //
 // CREATED:         10/14/2022
 //
-// LAST EDITED:     10/17/2022
+// LAST EDITED:     10/18/2022
 ////
 
+use serde::{Serialize, Deserialize};
+use wasm_bindgen::prelude::*;
 use yew::prelude::*;
+use web_sys::HtmlCanvasElement;
 
-#[function_component]
-pub fn BalanceHistory() -> Html {
-    html! {
-        <></>
+///////////////////////////////////////////////////////////////////////////////
+// ChartData
+////
+
+#[derive(Serialize, Deserialize)]
+pub struct ChartDataset {
+    label: String,
+
+    #[serde(rename = "backgroundColor")]
+    background_color: String,
+
+    #[serde(rename = "borderColor")]
+    border_color: String,
+
+    data: Vec<i32>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ChartData {
+    labels: Vec<String>,
+    datasets: Vec<ChartDataset>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ChartOptions {}
+
+#[derive(Serialize, Deserialize)]
+pub struct ChartConfig {
+    #[serde(rename = "type")]
+    chart_type: String,
+    data: ChartData,
+    options: ChartOptions,
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// ChartJS Bindings
+////
+
+#[wasm_bindgen]
+extern "C" {
+    type Chart;
+
+    #[wasm_bindgen(constructor)]
+    fn new(item: HtmlCanvasElement, config: JsValue) -> Chart;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// BalanceHistory
+////
+
+pub struct BalanceHistory {
+    chart: Option<Chart>,
+    canvas: NodeRef,
+}
+
+impl Component for BalanceHistory {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            chart: None,
+            canvas: NodeRef::default(),
+        }
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <>
+            <h2>{ "Account Balance History" }</h2>
+            <div>
+                <canvas ref={self.canvas.clone()}></canvas>
+            </div>
+            </>
+        }
+    }
+
+    fn rendered(&mut self, _ctx: &Context<Self>, first_render: bool) {
+        if first_render {
+            let labels = vec![
+                "January".to_string(),
+                "February".to_string(),
+                "March".to_string(),
+                "April".to_string(),
+                "May".to_string(),
+                "June".to_string(),
+            ];
+
+            let y_data = vec![0, 10, 5, 2, 20, 30, 45];
+
+            let data = ChartData {
+                labels,
+                datasets: vec![ ChartDataset {
+                    label: "My First dataset".to_string(),
+                    background_color: "rgb(255, 99, 132)".to_string(),
+                    border_color: "rgb(255, 99, 132)".to_string(),
+                    data: y_data,
+                }],
+            };
+
+            let chart_config = ChartConfig {
+                chart_type: "line".to_string(),
+                data,
+                options: ChartOptions {},
+            };
+
+            let config = serde_wasm_bindgen::to_value(&chart_config).unwrap();
+            web_sys::console::log_1(&config);
+            self.chart = Some(Chart::new(
+                self.canvas.cast::<HtmlCanvasElement>().unwrap(),
+                config
+            ));
+        }
     }
 }
 
