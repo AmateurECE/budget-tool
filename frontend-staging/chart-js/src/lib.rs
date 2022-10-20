2c4159822be4f0f9edf2f4213b4e1a2e46689778
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-10-19 22:40:06 -0500

    frontend-staging: Break ChartJS bindings out into separate crate

---
+++ frontend-staging/chart-js/src/lib.rs
@@ -0,0 +1,63 @@
+///////////////////////////////////////////////////////////////////////////////
+// NAME:            lib.rs
+//
+// AUTHOR:          Ethan D. Twardy <ethan.twardy@gmail.com>
+//
+// DESCRIPTION:     Wasm bindings for ChartJS.
+//
+// CREATED:         10/19/2022
+//
+// LAST EDITED:     10/19/2022
+////
+
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsValue;
+use web_sys::HtmlCanvasElement;
+
+///////////////////////////////////////////////////////////////////////////////
+// ChartData
+////
+
+#[derive(Serialize, Deserialize)]
+pub struct ChartDataset {
+    pub label: String,
+
+    #[serde(rename = "backgroundColor")]
+    pub background_color: String,
+
+    #[serde(rename = "borderColor")]
+    pub border_color: String,
+    pub data: Vec<i32>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ChartData {
+    pub labels: Vec<String>,
+    pub datasets: Vec<ChartDataset>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ChartOptions {}
+
+#[derive(Serialize, Deserialize)]
+pub struct ChartConfig {
+    #[serde(rename = "type")]
+    pub chart_type: String,
+    pub data: ChartData,
+    pub options: ChartOptions,
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// ChartJS Bindings
+////
+
+#[wasm_bindgen]
+extern "C" {
+    pub type Chart;
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(item: HtmlCanvasElement, config: JsValue) -> Chart;
+}
+
+///////////////////////////////////////////////////////////////////////////////
--- frontend-staging/frontend-staging/src/performance/account_balance.rs
+++ frontend-staging/frontend-staging/src/performance/account_balance.rs
@@ -7,59 +7,12 @@
 //
 // CREATED:         10/14/2022
 //
-// LAST EDITED:     10/18/2022
+// LAST EDITED:     10/19/2022
 ////
 
-use serde::{Serialize, Deserialize};
-use wasm_bindgen::prelude::*;
-use yew::prelude::*;
+use chart_js::{Chart, ChartConfig, ChartData, ChartDataset, ChartOptions};
 use web_sys::HtmlCanvasElement;
-
-///////////////////////////////////////////////////////////////////////////////
-// ChartData
-////
-
-#[derive(Serialize, Deserialize)]
-pub struct ChartDataset {
-    label: String,
-
-    #[serde(rename = "backgroundColor")]
-    background_color: String,
-
-    #[serde(rename = "borderColor")]
-    border_color: String,
-
-    data: Vec<i32>,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct ChartData {
-    labels: Vec<String>,
-    datasets: Vec<ChartDataset>,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct ChartOptions {}
-
-#[derive(Serialize, Deserialize)]
-pub struct ChartConfig {
-    #[serde(rename = "type")]
-    chart_type: String,
-    data: ChartData,
-    options: ChartOptions,
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// ChartJS Bindings
-////
-
-#[wasm_bindgen]
-extern "C" {
-    type Chart;
-
-    #[wasm_bindgen(constructor)]
-    fn new(item: HtmlCanvasElement, config: JsValue) -> Chart;
-}
+use yew::prelude::*;
 
 ///////////////////////////////////////////////////////////////////////////////
 // BalanceHistory
@@ -84,50 +37,48 @@ impl Component for BalanceHistory {
     fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
             <>
-            <h2>{ "Account Balance History" }</h2>
-            <div>
-                <canvas ref={self.canvas.clone()}></canvas>
-            </div>
-            </>
+                <h2>{ "Account Balance History" }</h2>
+                <div>
+                    <canvas ref={self.canvas.clone()}></canvas>
+                </div>
+                </>
         }
     }
 
-    fn rendered(&mut self, _ctx: &Context<Self>, first_render: bool) {
-        if first_render {
-            let labels = vec![
-                "January".to_string(),
-                "February".to_string(),
-                "March".to_string(),
-                "April".to_string(),
-                "May".to_string(),
-                "June".to_string(),
-            ];
+    fn rendered(&mut self, _ctx: &Context<Self>, _first_render: bool) {
+        let labels = vec![
+            "January".to_string(),
+            "February".to_string(),
+            "March".to_string(),
+            "April".to_string(),
+            "May".to_string(),
+            "June".to_string(),
+        ];
 
-            let y_data = vec![0, 10, 5, 2, 20, 30, 45];
+        let y_data = vec![0, 10, 5, 2, 20, 30, 45];
 
-            let data = ChartData {
-                labels,
-                datasets: vec![ ChartDataset {
-                    label: "My First dataset".to_string(),
-                    background_color: "rgb(255, 99, 132)".to_string(),
-                    border_color: "rgb(255, 99, 132)".to_string(),
-                    data: y_data,
-                }],
-            };
+        let data = ChartData {
+            labels,
+            datasets: vec![ ChartDataset {
+                label: "My First dataset".to_string(),
+                background_color: "rgb(255, 99, 132)".to_string(),
+                border_color: "rgb(255, 99, 132)".to_string(),
+                data: y_data,
+            }],
+        };
 
-            let chart_config = ChartConfig {
-                chart_type: "line".to_string(),
-                data,
-                options: ChartOptions {},
-            };
+        let chart_config = ChartConfig {
+            chart_type: "line".to_string(),
+            data,
+            options: ChartOptions {},
+        };
 
-            let config = serde_wasm_bindgen::to_value(&chart_config).unwrap();
-            web_sys::console::log_1(&config);
-            self.chart = Some(Chart::new(
-                self.canvas.cast::<HtmlCanvasElement>().unwrap(),
-                config
-            ));
-        }
+        let config = serde_wasm_bindgen::to_value(&chart_config).unwrap();
+        web_sys::console::log_1(&config);
+        self.chart = Some(Chart::new(
+            self.canvas.cast::<HtmlCanvasElement>().unwrap(),
+            config
+        ));
     }
 }
 
