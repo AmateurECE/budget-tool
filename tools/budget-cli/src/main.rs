8205bc235bfe0d2797fdbab3a633da89cdcdf862
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-11-09 07:48:47 -0600

    Remove old dependencies from budget-cli

--- tools/budget-cli/src/main.rs
+++ tools/budget-cli/src/main.rs
@@ -7,33 +7,10 @@
 //
 // CREATED:         09/22/2022
 //
-// LAST EDITED:     09/25/2022
+// LAST EDITED:     11/09/2022
 ////
 
-use budget_models::models::NewTransaction;
 use clap::Parser;
-use http::Uri;
-use reqwest::Client;
-use serde::Deserialize;
-use std::fs::File;
-
-///////////////////////////////////////////////////////////////////////////////
-// Partial New Transactions--Schema for pre-imported data
-////
-
-#[derive(Deserialize)]
-struct PartialNewTransaction {
-    account: String,
-    description: String,
-    date: String,
-    amount: f64,
-}
-
-impl Into<NewTransaction> for PartialNewTransaction {
-    fn into(self) -> NewTransaction {
-        todo!()
-    }
-}
 
 ///////////////////////////////////////////////////////////////////////////////
 // Main
@@ -54,24 +31,6 @@ struct Args {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-    let client = Client::builder().user_agent("budget-import").build()?;
-
-    let transactions: Vec<PartialNewTransaction> =
-        serde_json::from_reader(File::open(&args.file)?)?;
-    let endpoint = Uri::builder()
-        .scheme("https")
-        .authority(args.host.as_str())
-        .path_and_query("/api/transactions")
-        .build()?
-        .to_string();
-    for transaction in transactions {
-        let new_transaction: NewTransaction = transaction.into();
-        client
-            .post(&endpoint)
-            .body(serde_json::to_string(&new_transaction)?)
-            .send()
-            .await?;
-    }
 
     Ok(())
 }
