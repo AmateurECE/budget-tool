e63a461cabff068d0eff8d323056ba8fc8ed41b8
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-11-09 22:11:54 -0600

    Implement budget listing subcommand
    ...with some pretty atrocious table printing logic

--- tools/budget-cli/src/main.rs
+++ tools/budget-cli/src/main.rs
@@ -11,12 +11,13 @@
 ////
 
 use budget_backend_lib::prelude::*;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use sea_orm::prelude::*;
-use sea_orm::Database;
+use sea_orm::{Database, DatabaseConnection};
+use std::cmp;
 
 ///////////////////////////////////////////////////////////////////////////////
-// Main
+// CLI
 ////
 
 #[derive(Parser)]
@@ -25,16 +26,129 @@ struct Args {
     /// Database URL
     #[clap(short, long, value_parser)]
     pub url: String,
+
+    #[command(subcommand)]
+    pub object: Object,
 }
+
+#[derive(Subcommand)]
+enum Object {
+    /// Actions available on the set of periodic budgets
+    Periodic {
+        /// List periodic budgets in the database
+        #[command(subcommand)]
+        verb: Verb,
+    },
+}
+
+#[derive(Subcommand)]
+enum Verb {
+    List,
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Cheap Table Printing Solution
+////
+
+const PADDING_LENGTH: usize = 4;
+
+fn print_table(data: Vec<Vec<String>>, headers: Vec<String>) {
+    let row_length = data
+        .iter()
+        .map(|column| {
+            let max = cmp::max(
+                column
+                    .iter()
+                    .map(|element| element.len())
+                    .max()
+                    .unwrap_or(0),
+                headers.first().map(|h| h.len()).unwrap_or(0),
+            );
+            if 0 == max % PADDING_LENGTH {
+                max
+            } else {
+                max + (PADDING_LENGTH - (max % PADDING_LENGTH))
+            }
+        })
+        .collect::<Vec<usize>>();
+
+    let sum: usize = row_length.iter().sum();
+    let mut output = headers
+        .iter()
+        .enumerate()
+        .map(|(i, header)| {
+            header.to_owned()
+                + &(0..(row_length[i] - header.len()))
+                    .map(|_| " ")
+                    .collect::<String>()
+        })
+        .collect::<String>()
+        + "\n";
+    output += &(0..sum).map(|_| "-").collect::<String>();
+    output += "\n";
+
+    for i in 0..data[0].len() {
+        for j in 0..data.len() {
+            output += &data[j][i];
+            output += &(0..(row_length[j] - data[j][i].len()))
+                .map(|_| " ")
+                .collect::<String>();
+        }
+    }
+    println!("{}", output);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// PeriodicBudget Operations
+////
+
+async fn periodic_budget(
+    verb: &Verb,
+    db: &DatabaseConnection,
+) -> anyhow::Result<()> {
+    match &verb {
+        Verb::List => {
+            let budgets: Vec<periodic_budgets::Model> =
+                PeriodicBudgets::find().all(db).await?;
+            let output = vec![
+                budgets
+                    .iter()
+                    .map(|budget| budget.id.to_string())
+                    .collect::<Vec<String>>(),
+                budgets
+                    .iter()
+                    .map(|budget| budget.start_date.to_string())
+                    .collect::<Vec<String>>(),
+                budgets
+                    .iter()
+                    .map(|budget| budget.end_date.to_string())
+                    .collect::<Vec<String>>(),
+            ];
+            print_table(
+                output,
+                vec![
+                    "Id".to_string(),
+                    "Start Date".to_string(),
+                    "End Date".to_string(),
+                ],
+            );
+        }
+    }
+
+    Ok(())
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Main
+////
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let db = Database::connect(args.url).await?;
-    let budgets: Vec<periodic_budgets::Model> =
-        PeriodicBudgets::find().all(&db).await?;
-    println!("{:?}", &budgets);
-    Ok(())
+    match &args.object {
+        Object::Periodic { verb } => periodic_budget(verb, &db).await,
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
